677ff9cb33f8f24044b91de88d9754739af506a6
ac141102 <claudius.haag@mib.uni-stuttgart.de>
2024-08-14 10:46:06 +0200

    Pixi ready (#3)
    * Create pixi
    + move all python code in MSUtil folder (package)
    
    * Make pixi work
    + Change all imports to be super specific -> could be done easier
    
    * Add Task and README

--- MSUtils/junk/voronoi3D.py
+++ MSUtils/junk/voronoi3D.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import subprocess
-from voronoi_helpers import *
+from MSUtils.voronoi.voronoi_helpers import *
 
 if __name__ == '__main__':
     # Define parameters for the Voronoi tessellation
@@ -17,15 +17,15 @@ if __name__ == '__main__':
     seeds, lattice_vectors = generate_seeds(num_crystals, RVE_length, "random")
     neighbors, _ = find_neighbors(seeds, RVE_length)
     tessellation, seeds = generate_periodic_voronoi(Nx, Ny, Nz, num_crystals, RVE_length, seeds)
-    save_sample_to_hdf5("/dset_0", tessellation=tessellation, filename=h5filename, seeds=seeds, neighbors=neighbors)    
+    save_sample_to_hdf5("/dset_0", tessellation=tessellation, filename=h5filename, seeds=seeds, neighbors=neighbors)
 
     tessellation, seeds = generate_periodic_voronoi(Nx*4, Ny*4, Nz*4, num_crystals, RVE_length, seeds)
     save_sample_to_hdf5("/dset_1", tessellation=tessellation, filename=h5filename, seeds=seeds, neighbors=neighbors)
-    
+
     # # Erode the loaded tessellation.
     # shrink_factor = 3
     # shrunk, GBVoxelInfo = shrink_crystals_periodically(tessellation, seeds, neighbors, shrink_factor, RVE_length)
-    # save_sample_to_hdf5("/dset_0/eroded_"+str(shrink_factor), tessellation=shrunk, filename=h5filename, seeds=seeds, neighbors=neighbors, GBVoxelInfo_list=GBVoxelInfo)    
+    # save_sample_to_hdf5("/dset_0/eroded_"+str(shrink_factor), tessellation=shrunk, filename=h5filename, seeds=seeds, neighbors=neighbors, GBVoxelInfo_list=GBVoxelInfo)
 
     subprocess.run(["python", "h52xdmf.py", "-v", h5filename], check=True)
 
--- MSUtils/voronoi/VoronoiImage.py
+++ MSUtils/voronoi/VoronoiImage.py
@@ -1,8 +1,8 @@
 import numpy as np
 from scipy.spatial import cKDTree as KDTree
 import sys, os, numpy as np
-sys.path.append(os.path.join('general/'))
-from MicrostructureImage import MicrostructureImage
+# sys.path.append(os.path.join('general/'))
+from MSUtils.general.MicrostructureImage import MicrostructureImage
 
 class PeriodicVoronoiImage(MicrostructureImage):
     def __init__(self, N = None, seeds = None, RVE_length=[1, 1, 1], h5_filename=None, dset_name=None):
@@ -18,8 +18,8 @@ class PeriodicVoronoiImage(MicrostructureImage):
 
     def _generate_periodic_voronoi(self, N, L):
         # Adjust for voxel centers and create a mesh grid of points within the main domain
-        grid_ranges = [np.linspace(0.5 * L[dim] / N[dim], 
-                                   L[dim] - 0.5 * L[dim] / N[dim], 
+        grid_ranges = [np.linspace(0.5 * L[dim] / N[dim],
+                                   L[dim] - 0.5 * L[dim] / N[dim],
                                    N[dim]) for dim in range(len(L))]
         meshgrid = np.meshgrid(*grid_ranges, indexing='ij')
         points = np.column_stack([axis.ravel() for axis in meshgrid])
