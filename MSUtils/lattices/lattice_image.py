4d2da28410f28448a72d85e1afb4385c9f947242
ac133718 <ac133718@uni-stuttgart.de>
2024-09-06 14:32:06 +0200

    minor changes

--- MSUtils/lattices/lattice_image.py
+++ MSUtils/lattices/lattice_image.py
@@ -1,27 +1,9 @@
 import numpy as np
 from lattice_definitions import *
-import sys, os
-# sys.path.append(os.path.join('general/'))
 from MSUtils.general.MicrostructureImage import MicrostructureImage
 
 def physical_to_voxel(point, dimensions, shape):
     return np.round(point / dimensions * (np.array(shape) - 1)).astype(int)
-
-def rotation_matrix_from_vectors(vec1, vec2):
-    """ Find the rotation matrix that aligns vec1 to vec2
-    :param vec1: A 3d "source" vector
-    :param vec2: A 3d "destination" vector
-    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
-    """
-    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
-    v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    I = np.identity(3)
-    if s == 0:
-        return I  # If vec1 and vec2 are already aligned
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    return I + kmat + np.dot(kmat, kmat) * ((1 - c) / (s ** 2))
 
 def draw_strut(microstructure, start, end, radius, voxel_sizes, strut_type, L):
     start = np.array(start)
@@ -49,16 +31,6 @@ def draw_strut(microstructure, start, end, radius, voxel_sizes, strut_type, L):
                         ((yy - y) * voxel_sizes[1]) ** 2 +
                         ((zz - z) * voxel_sizes[2]) ** 2)
             mask = distance <= radius ** 2
-
-        elif strut_type == 'square': ## Not very stable.. not correct in the presence of anisotropy
-            xx, yy, zz = np.meshgrid(np.arange(x_min, x_max), np.arange(y_min, y_max), np.arange(z_min, z_max), indexing='ij')
-            side_length = 2 * radius
-            local_coords = np.stack([xx - x, yy - y, zz - z], axis=-1)
-            rotation_matrix = rotation_matrix_from_vectors(direction, np.array([0, 0, 1]))
-            local_coords = np.tensordot(local_coords, rotation_matrix.T, axes=([3], [0]))
-
-            mask = (abs(local_coords[..., 0] * voxel_sizes[0]) <= side_length / 2) & \
-                   (abs(local_coords[..., 1] * voxel_sizes[1]) <= side_length / 2)
 
         microstructure[x_min:x_max, y_min:y_max, z_min:z_max][mask] = 1
 
@@ -121,6 +93,8 @@ if __name__ == "__main__":
         microstructures[name] = MicrostructureImage(image=image, metadata=tmp_metadata)
         microstructures[name].write(h5_filename='data/lattice_microstructures.h5', dset_name=name)
 
+    from MSUtils.general.h52xdmf import write_xdmf
+    write_xdmf('data/lattice_microstructures.h5', 'data/lattice_microstructures.xdmf', L, False, None, True)
     # vertices, edges = octet_truss_lattice()
     # plot_lattice(vertices, edges)
     # print(check_rigidity(vertices, edges))
