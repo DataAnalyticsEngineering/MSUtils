66c33f8187d567997c62738f3a85f5341e200f21
Claudius Haag <claudius.haag@mib.uni-stuttgart.de>
2024-10-08 16:26:03 +0200

    Actual main function and testing + Testing not working currently! Build works + v0.1.1 + add tasks for building and pushing to testpypi

--- MSUtils/voronoi/main.py
+++ MSUtils/voronoi/main.py
@@ -23,9 +23,7 @@ from MSUtils.general.h52xdmf import write_xdmf
 from MSUtils.voronoi.VoronoiGBErosion import PeriodicVoronoiImageErosion
 import numpy as np
 
-# %%
-if __name__ == "__main__":
-    
+def main():
     num_crystals = 8
     L = [1, 1, 1]
 
@@ -35,17 +33,22 @@ if __name__ == "__main__":
     voroTess = PeriodicVoronoiTessellation(L, SeedInfo.seeds)
     voroTess.write_to_vtu("data/voroTess.vtu")
 
-    Nx, Ny, Nz = 64, 64, 64    
+    Nx, Ny, Nz = 64, 64, 64
     voroImg = PeriodicVoronoiImage([Nx, Ny, Nz], SeedInfo.seeds, L)
     voroImg.write(h5_filename="data/voroImg.h5", dset_name="/dset_0", order="zyx")
     write_xdmf("data/voroImg.h5", "data/voroImg.xdmf", microstructure_length=[1,1,1])
-    
+
     voroErodedImg = PeriodicVoronoiImageErosion(voroImg, voroTess, shrink_factor=2)
     voroErodedImg.write("/dset_0", "data/voroImg_eroded.h5", order="zyx")
     write_xdmf("data/voroImg_eroded.h5", "data/voroImg_eroded.xdmf", microstructure_length=[1,1,1])
-    
-    
+
+
     msimage = MicrostructureImage(image=voroErodedImg.eroded_image, L=L)
     print(msimage.volume_fractions)
+
+# %%
+if __name__ == "__main__":
+    main()
+
 
 # %%
