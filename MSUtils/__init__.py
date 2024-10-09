a48ff03fb7d1293ca1bed118afa3692c6e894531
Claudius Haag <claudius.haag@mib.uni-stuttgart.de>
2024-10-09 22:12:53 +0200

    Clear repo for hosting website

--- MSUtils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Package Docstring
-"""
-
-# from . import general as general
-# from . import junk as junk
-# from . import lattices as lattices
-# from . import test as test
-# from . import voronoi as voronoi
--- test/test_main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import sys
-from MSUtils.voronoi.main import main
-
-def test_path():
-    [print(i) for i in sys.path]
-
-def test_1():
-    main()
